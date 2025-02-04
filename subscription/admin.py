7e18d75b7f65fd46fb613464520d3fa96e82af11
ZaidArman <zahidu864@gmail.com>
2025-02-04 20:42:31 +0500

    Subscription APIs (Incomplete)

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from subscription.models import SubscriptionPlan, Payment
+
 # Register your models here.
+@admin.register(SubscriptionPlan)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+@admin.register(Payment)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['stripe_checkout_id']
